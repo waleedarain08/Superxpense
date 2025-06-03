46fa9561896eedf228e76a1dd4816fdb803793e5
Waleed Arain <77847857+waleedarain08@users.noreply.github.com>
2025-06-04 03:25:34 +0500

    fixes

--- src/utilis/Constant.jsx
+++ src/utilis/Constant.jsx
@@ -1,7 +1,7 @@
-// export const baseUrl = 'https://superxpnse-be.onrender.com/';
-export const baseUrl = 'https://superxpnsebe.dev.cntxt.tools/';
+//export const baseUrl = 'https://superxpnsebe.cntxt.tools/'; // production
+export const baseUrl = 'https://superxpnsebe.dev.cntxt.tools/'; //development
 export const leanAppToken = '9c7c7f5f-5258-4b49-a498-dba6375a8f86'; // production
-//export const leanAppToken = '6dda512f-8201-488a-9884-ae7236615282'; // sandbox
+//export const leanAppToken = '6dda512f-8201-488a-9884-ae7236615282'; // development
 export const isSandbox = false; // set to true for sandbox environment
 //lean initialization is in IssueingCountryScreen.jsx and AccountsScreen.jsx
 
